928e685047ac01ed31e574c594db04a3fba16a65
Farahrehim97 <95203723+Farahrehim97@users.noreply.github.com>
2022-06-12 20:11:29 -0700

    Add files via upload

---
+++ co4/q11_co4.java
@@ -0,0 +1,18 @@
+package oopslab;
+
+import java.util.LinkedList;
+
+public class q11_co4 {
+public static void main(String args[])
+{
+	LinkedList<Integer> l=new LinkedList<Integer>();
+	l.add(12);
+	l.add(13);
+	l.add(44);
+	l.add(54);
+	System.out.println(l);
+	l.clear();
+	l.add(84);
+	System.out.println(l);
+}
+}
